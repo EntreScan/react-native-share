6e07453b78f7676131502a05e4eb3b6ca7dbb5ec
George Farro <gtfargo@users.noreply.github.com>
2016-02-12 12:32:49 -0600

    again...

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,7 @@
 'use strict';
 
 var NativeRNShare = require('react-native').NativeModules.RNShare;
-var NativeRNShare = NativeModules.RNShare;
+// var NativeRNShare = NativeModules.RNShare;
 
 /**
  * High-level docs for the RNShare iOS API can be written here.
